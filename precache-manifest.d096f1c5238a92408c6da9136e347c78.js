fa4e26ea7cb54a48490325b68f9782c1808a9cb2
Michael <michael@I.AM.MIKE>
2019-03-17 20:03:27 -0400

    Updates

---
+++ precache-manifest.d096f1c5238a92408c6da9136e347c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testone/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7bd6a28d399e1c2d0e17",
+    "url": "/testone/static/js/runtime~main.18b8bafd.js"
+  },
+  {
+    "revision": "c84bf49d7a2f8e10ef27",
+    "url": "/testone/static/js/main.7ee6cfa5.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/testone/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "c84bf49d7a2f8e10ef27",
+    "url": "/testone/static/css/main.f0075383.chunk.css"
+  },
+  {
+    "revision": "3cb06aeec481e05885cc5c5196e2d818",
+    "url": "/testone/index.html"
+  }
+];
